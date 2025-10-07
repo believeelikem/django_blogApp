02f05ddd9188c4981ce6122e7b8114ebb82d2f70
Believe Elikem <ayamwenzdayz@gmail.com>
2025-10-07 04:34:21 +0000

    Adding pagination to home and user_page

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
 from .models import Post
 from django.views.generic import (
     ListView,
@@ -20,6 +21,19 @@ class PostListView(ListView):
     template_name = "blogapp/home.html"
     context_object_name = "posts"
     ordering = ["-date_posted"]
+    paginate_by = 5 
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blogapp/user_post.html"
+    context_object_name = "posts"
+    paginate_by = 5 
+    
+    def get_queryset(self):
+        user = get_object_or_404(User,username=self.kwargs.get("username"))
+        return Post.objects.filter(author = user).order_by("-date_posted")
+    
 
 class PostDetailView(DetailView):
     model = Post
